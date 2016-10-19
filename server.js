0f3d253591b6beb15739078b7450581249eaaf96
Talon Hughes <talon.hughes@gmail.com>
2016-10-18 18:44:13 -0600

    changes

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express = require('express');
 var app = express();
 
 app.listen(8887, function(){
-  console.log('Listening on port 8887');
+  console.log('Listening on port 8887 --- YEEEE BOIIII');
 });
 
 var messages = ["Hello there.", "I'm sorry, I cannot take any requests at this time.", "I can tell you how to do that."];
